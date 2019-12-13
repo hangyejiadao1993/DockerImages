2ddbac53522e1f4d7e6e8c9e34cc83e36879d5d2
hangyejiadao1993 <1139258234@qq.com>
2019-12-13 08:07:19 +0800

    123123

--- flannel.sh
+++ flannel.sh
@@ -1,6 +1,6 @@
 
 sudo docker pull registry.cn-shenzhen.aliyuncs.com/hang1993/amd64:v0.11.0 
-docker tag registry.cn-shenzhen.aliyuncs.com/hang1993/amd64:v0.11.0   quay.io/coreos/flannel:v0.11.0-arm
+docker tag registry.cn-shenzhen.aliyuncs.com/hang1993/amd64:v0.11.0   quay.io/coreos/flannel:v0.11.0-amd64
 
 
 sudo docker pull registry.cn-shenzhen.aliyuncs.com/hang1993/flannelarm:v0.11.0
